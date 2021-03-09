1227592f6f46c477cd481cfc73b19a92284bf442
Jose Roberto Ribeiro Junior <betopr18@gmail.com>
2021-03-09 01:43:24 -0300

    First Commit

---
+++ canvas.js
@@ -0,0 +1,39 @@
+window.addEventListener('load', ()=>{
+    const canvas = document.querySelector("#canvas");
+    const context = canvas.getContext("2d");
+
+    //Resizing
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+    //Variables
+    let painting = false;
+
+    function startPosition(e){
+        painting = true;
+        draw(e);
+    }
+    function finishedPosition(){
+        painting = false;
+        context.beginPath();
+    }
+
+    function draw(e){
+        if(!painting) return;
+        context.lineWidth = 10;
+        context.lineCap = "round";
+
+        context.lineTo(e.clientX, e.clientY);
+        context.stroke();
+        context.beginPath();
+        context.moveTo(e.clientX, e.clientY);
+    }
+
+    //EventListeners
+    canvas.addEventListener("mousedown", startPosition);
+    canvas.addEventListener("mouseup", finishedPosition);
+    canvas.addEventListener("mousemove", draw);
+
+
+});
+
